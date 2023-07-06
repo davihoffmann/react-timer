8612fbbfe87e5fb4f1013b499cf07e5541631057
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2023-07-06 20:11:52 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,62 @@
+import { Play } from 'phosphor-react'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            type="text"
+            placeholder="De um nome para o seu projeto"
+            list="task-sugestions"
+          />
+
+          <datalist id="task-sugestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="Projeto 4"></option>
+            <option value="Teste"></option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">Durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit" disabled>
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
