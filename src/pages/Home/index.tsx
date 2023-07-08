07c718e274e5a74edd97afcb136b9c3494ff53a3
Davi Hoffmann <davi.phoffmann@gmail.com>
2023-07-08 17:17:59 -0300

    desenvolvido contador do ciclo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { differenceInSeconds } from 'date-fns'
 
 import {
   CountdownContainer,
@@ -12,7 +14,6 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
-import { useCallback, useMemo } from 'react'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -24,6 +25,13 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+}
+
 export function Home() {
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -33,13 +41,88 @@ export function Home() {
     },
   })
 
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  // pega o ciclo ativo
+  const activeCycle = useMemo(
+    () => cycles.find((c) => c.id === activeCycleId),
+    [activeCycleId, cycles],
+  )
+
+  // pega o total de segundos do ciclo ativo
+  const totalSeconds = useMemo(
+    () => (activeCycle ? activeCycle.minutesAmount * 60 : 0),
+    [activeCycle],
+  )
+  // calcula o segundo total menos a quantidade de segundos que já passou
+  const currentSeconds = useMemo(
+    () => (activeCycle ? totalSeconds - amountSecondsPassed : 0),
+    [activeCycle, amountSecondsPassed, totalSeconds],
+  )
+
+  // converte os segundos em minutos
+  const minutesAmount = useMemo(
+    () => Math.floor(currentSeconds / 60),
+    [currentSeconds],
+  )
+  // pega a quantidade de segundos do resto da divisão da conversão de minutos
+  const secondsAmount = useMemo(() => currentSeconds % 60, [currentSeconds])
+
+  // transforma os minutos para string e concatena um zero quando estiver apenas 1 caracter
+  const minutes = useMemo(
+    () => `${minutesAmount}`.padStart(2, '0'),
+    [minutesAmount],
+  )
+  // transforma os segundos para string e concatena um zero quando estiver apenas 1 caracter
+  const seconds = useMemo(
+    () => `${secondsAmount}`.padStart(2, '0'),
+    [secondsAmount],
+  )
+
+  // inicia a contagem do ciclo
+  useEffect(() => {
+    let interval: number
+    if (activeCycle) {
+      interval = setInterval(() => {
+        // compara a data atual com a data de inicio do ciclo para ter a conategem de segundo mais precisa
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [activeCycle])
+
   const onSubmit = useCallback(
     (data: NewCycleFormData) => {
-      console.log(data)
+      const id = new Date().getTime().toString()
+
+      const newCycle: Cycle = {
+        id,
+        task: data.task,
+        minutesAmount: data.minutesAmount,
+        startDate: new Date(),
+      }
+
+      setCycles((prevState) => [...prevState, newCycle])
+      setActiveCycleId(id)
+      setAmountSecondsPassed(0)
+
       reset()
     },
     [reset],
   )
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [activeCycle, minutes, seconds])
 
   const task = watch('task')
   const isSubmitDisabled = useMemo(() => {
@@ -82,11 +165,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
